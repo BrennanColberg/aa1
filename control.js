725723d673ff6c400400e22daaf88bf81d71a7c4
Brennan Colberg <brennan.colberg@gmail.com>
2018-08-05 08:02:18 -0700

    Comments!

--- control.js
+++ control.js
@@ -1,57 +1,79 @@
 "use strict";
 (function() {
 	
+	// simple function abbreviations that shorten my code nicely
 	function $(id) { return document.getElementById(id); }
 	function hide(dom) { dom.classList.add("hidden"); }
 	function show(dom) { dom.classList.remove("hidden"); }
 	
+	// declaring module-global variables
 	let data = undefined;
 	let anthem = undefined;
 	let timer = undefined;
 	
+	// ultimate starting loading function
 	window.addEventListener("load", function() {
+		// loads JS to buttons
 		$("control").onclick = function() { ajaxGET("default.json", loadGame); };
 		$("resume").onclick = function() { loadGame(loadCookie("data")); }
 		$("next").onclick = next;
 		$("back").onclick = back;
+		// shows "resume" button if there's a save
 		if (cookieExists("data")) show($("resume"));
+		// adds more event listeners (for saving, keys, etc)
 		document.addEventListener("keydown", pressKey);
 		window.addEventListener("beforeunload", saveData);
 		window.addEventListener("blur", saveData);
 	});
 	
+	// loads the given JSON as the game state (default or save)
 	function loadGame(json) {
 		data = JSON.parse(json);
 		updateDisplay();
 		startAnthem();
-		hide($("resume"));
+		hide($("resume")); // no need to "resume" anymore
+		// I do this weirdly because play references "this"
+		// and so it needs to be called by a click not a direct
+		// method invocation
 		$("control").onclick = play;
 		$("control").click();
 	}
 	
+	// saves game state to a JSON-format cookie (which is notably
+	// not a good way to store data, but I'm hosting on GitHub
+	// Pages to cheap out on server space so it's fine I guess)
 	function saveData() {
-		if (data) saveCookie("data", JSON.stringify(data));
+		// logic check to avoid saving blank data and screwing
+		// everything up
+		if (data) {
+			saveCookie("data", JSON.stringify(data));
+		}
 	}
 	
+	// pauses the game (stops timer, stops music, etc)
 	function pause() {
 		anthem.pause();
 		hide($("next"));
 		hide($("back"));
 		clearInterval(timer);
+		// toggling "pause" button to "play" button
 		this.textContent = "Play";
 		this.onclick = play;
 	}
-	
+
+	// starts/resumes/"play"s the game (starts timer, starts music)
 	function play() {
 		anthem.play()
 		show($("info"));
 		show($("next"));
 		show($("back"));
 		timer = setInterval(updateTime, data.MEASURED_TIME_INTERVAL);
+		// toggling "play" button to "pause" button
 		this.textContent = "Pause";
 		this.onclick = pause;
 	}
 	
+	// tracks key presses and lets them do things
 	function pressKey(event) {
 		let key = event.keyCode;
 		if (key === 32) 
@@ -64,22 +86,27 @@
 
 	}
 	
+	// loads the current country's name and flag
 	function updateDisplay() {
 		$("title").textContent = data.countries[data.current.index].name;
 		$("flag").src = data.countries[data.current.index].flag;
 		printTime();
 	}
 	
+	// starts playing the current country's anthem
 	function startAnthem() {
+		// stops currently playing anthem if it exists
 		if (anthem) anthem.pause();
 		anthem = new Audio(data.countries[data.current.index].anthem);
 		anthem.loop = true;
+		// resumes correct time loading from a save?
 		anthem.addEventListener('loadedmetadata', function() {
     		this.currentTime = (data.current.time / 1000.0) % this.duration;
     		this.play(); 
 		});
 	}
 	
+	// goes to the next country's turn
 	function next() {
 		data.current.index = wrap(data.current.index + 1, 0, data.countries.length - 1);
 		updateDisplay();
@@ -87,6 +114,7 @@
 		startAnthem();
  	}
 	
+	// goes to the last country's turn
 	function back() {
 		data.current.index = wrap(data.current.index - 1, 0, data.countries.length - 1);
 		updateDisplay();
@@ -94,6 +122,8 @@
 		startAnthem();
 	}
 	
+	// standard method for wrapping numbers (confines to boundaries
+	// but then adds excess in a periodic fashion)
 	function wrap(num, min, max) {
 		let r = max - min + 1;
 		while(num > max) num -= r;
@@ -101,22 +131,26 @@
 		return num;
 	}
 	
+	// resets the current time
 	function resetTime() {
 		data.current.time = 0;
 		printTime();
 	}
 	
+	// updates the timer
 	function updateTime() {
 		data.current.time += data.MEASURED_TIME_INTERVAL;
 		data.countries[data.current.index].elapsed_time += data.MEASURED_TIME_INTERVAL;
 		printTime();
 	}
 	
+	// prints the time to the screen
 	function printTime() {
 		$("currentTime").textContent = timeString(data.current.time);
 		$("totalTime").textContent = timeString(data.countries[data.current.index].elapsed_time);
 	}
 	
+	// turns an int millisecond measurement into a 00:00:00 clock
 	function timeString(time) {
 		let result = "";
 		time = Math.floor(time);
