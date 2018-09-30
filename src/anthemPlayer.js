caf0eeb448bd172eb79f5612d5dde019734006a3
Brennan Colberg <brennan.colberg@gmail.com>
2018-09-29 18:35:11 -0700

    Fixed audio, finally!
    still need to put in .ogg files

--- src/anthemPlayer.js
+++ src/anthemPlayer.js
@@ -3,41 +3,44 @@
 // in index.js, where it is used; the code is simply separated off here.
 
 function AnthemPlayer() {
-	
+
 	// currently playing anthem
 	let current = undefined;
 	// audio object
-	let dom = document.createElement("audio");
-		dom.loop = true;
-	
+	window.addEventListener("load", function () {
+		let audioDOM = document.getElementById("anthemPlayer");
+	});
+
 	// sets the anthem player to use a new file, which changes the anthem being
 	// played!
-	this.setFile = function(file) {
+	this.setFile = function (file) {
 		current = file;
 	}
-	
+
 	// starts playing the current country's anthem, given file name
 	// additionally, there's an optional argument to specify at what point
 	// in the song it should start playing
-	this.start = function(file = current, time = 0.0) {
-		if (file !== dom.src) {
-			dom.src = file;
-			dom.onloadedmetadata = function() {
-				console.log(dom.duration);
-				dom.currentTime = (time / 1000.0) % dom.duration;
-				dom.play(); // gets blocked by Safari as an auto-player
-			}
+	this.start = function (file = current, time = 0.0) {
+		let sourceDOM = document.getElementById("anthemSource");
+		console.log(sourceDOM);
+		let audioDOM = document.getElementById("anthemPlayer");
+		if (file !== sourceDOM.src) {
+			sourceDOM.src = file;
+			//audioDOM.currentTime = (time / 1000.0) % sourceDOM.duration;
+			audioDOM.load();
+			audioDOM.play();
 		}
+
 	}
-	
+
 	// resumes the music
-	this.play = function() {
-		dom.play();
+	this.play = function () {
+		audioDOM.play();
 	}
-	
+
 	// pauses the music
-	this.pause = function() {
-		dom.pause();
+	this.pause = function () {
+		audioDOM.pause();
 	}
-	
+
 }
